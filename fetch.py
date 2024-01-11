4a493510691d3f9c8da01623e322835ab7325b21
Adrienne Stilp <amstilp@uw.edu>
2024-01-11 15:25:44 -0800

    Download files into a temporary directory
    They can then be copied to the final directory.

--- fetch.py
+++ fetch.py
@@ -1,6 +1,11 @@
+"""Script to fetch files from dbGaP using sratoolkit prefetch with a kart file."""
+
 import argparse
+import os
+import shutil
 import subprocess
 import sys
+import tempfile
 
 
 class dbGaPFileFetcher:
@@ -12,11 +17,11 @@ class dbGaPFileFetcher:
             ngc_file (str): The path to the ngc file containing the project key.
             prefetch (str): The path to the prefetch executable.
         """
-        self.ngc_file = ngc_file
-        self.prefetch = prefetch
-        self.output_dir = output_dir
+        self.ngc_file = os.path.abspath(ngc_file)
+        self.prefetch = os.path.abspath(prefetch)
+        self.output_dir = os.path.abspath(output_dir)
 
-    def download_files(self, cart, manifest, n_retries=5):
+    def download_files(self, cart, manifest, n_retries=3):
         """Download files from dbGaP using a kart file. Because prefetch sometimes fails to download a file
         but does not report an error, this method will retry downloading the cart a number of times.
 
@@ -25,10 +30,19 @@ class dbGaPFileFetcher:
             manifest (str): The path to the manifest file.
             n_retries (int): The number of times to retry downloading the cart.
         """
-        for i in range(n_retries):
-            print(i)
-            print("Attempt {}/{} to download files.".format(i + 1, n_retries))
-            self._run_prefetch(cart)
+        # Work in a temporary directory to do the downloading.
+        cart_file = os.path.abspath(cart)
+        original_working_directory = os.getcwd()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            os.chdir(temp_dir)
+            # Download the files
+            for i in range(n_retries):
+                print("Attempt {}/{} to download files.".format(i + 1, n_retries))
+                self._run_prefetch(cart_file)
+            # Copy files to the output directory
+            os.chdir(original_working_directory)
+            shutil.copytree(temp_dir, self.output_dir, dirs_exist_ok=True)
+
 
     def _run_prefetch(self, cart_file):
         """Run the prefetch command to download files from dbGaP."""
