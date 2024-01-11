e27fbb357b8fb58a4410174fc9a086da4e717a76
Adrienne Stilp <amstilp@uw.edu>
2024-01-11 14:39:28 -0800

    Add class to handle downloading files from dbGaP
    Instead of having a number of methods to download files that pass
    infomration around, define a class that will store this information
    and know how to download everything.

--- fetch.py
+++ fetch.py
@@ -1,4 +1,39 @@
 import argparse
+import subprocess
+import sys
+
+
+class dbGaPFileFetcher:
+
+    def __init__(self, ngc_file, prefetch, cart):
+        """Initialize the dbGaPFileFetcher.
+
+        Args:
+            ngc_file (str): The path to the ngc file containing the project key.
+            prefetch (str): The path to the prefetch executable.
+        """
+        self.ngc_file = ngc_file
+        self.prefetch = prefetch
+        self.cart = cart
+        # self.manifest = manifest
+
+    def download_files(self):
+        """Download files from dbGaP using a kart file.
+
+        Args:
+            cart (str): The path to the cart file.
+            manifest (str): The path to the manifest file.
+            verbose (bool): Print more information.
+        """
+        cmd = "{prefetch} --ngc {ngc} --order kart --cart {cart}".format(
+            prefetch=self.prefetch,
+            ngc=self.ngc_file,
+            cart=self.cart,
+        )
+        print(cmd)
+
+        returned_value = subprocess.call(cmd, shell=True)
+        sys.exit(returned_value)
 
 
 if __name__ == "__main__":
@@ -18,3 +53,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print("hello world!")
+
+    fetcher = dbGaPFileFetcher(args.ngc, args.prefetch, args.cart)
+    fetcher.download_files()
